a857331e3bc1793027f1836a2290b56ecb88d158
Zachary G Nicolaou <zachary.nicolaou@gibbshead.(none)>
2020-10-27 16:23:45 -0400

    updated resolvants

--- count.py
+++ count.py
@@ -4,10 +4,10 @@ c=2
 bins=501
 bmax=10
 
-for c in range(2,6):
+for c in range(2,3):
     counts=np.zeros([bins,bins])
     tot=0
-    for seed in range(10000):
+    for seed in range(1024):
         try:
             vals=np.load("data/"+str(c)+"/"+str(seed)+"evals.npy")
             counts=counts+np.histogram2d(np.real(vals),np.imag(vals),bins=bins,range=[[-bmax,bmax],[-bmax,bmax]])[0]
--- rmt.py
+++ rmt.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     parser.add_argument("--zr0", type=float, default=0, dest='zr0', help='Initial Re(z) for generalized resolvant')
     parser.add_argument("--zi0", type=float, default=0, dest='zi0', help='Initial Im(z) for generalized resolvant')
     parser.add_argument("--zr1", type=float, default=0, dest='zr1', help='Final Re(z) for generalized resolvant')
-    parser.add_argument("--zi1", type=float, default=1, dest='zi1', help='Final Im(z) for generalized resolvant')
+    parser.add_argument("--zi1", type=float, default=10, dest='zi1', help='Final Im(z) for generalized resolvant')
     parser.add_argument("--gnum", type=int, default=25, dest='gnum', help='Number of g to evaluate')
     parser.add_argument("--eta", type=float, default=1e-2, dest='eta', help='Regularization parameter')
     args = parser.parse_args()
