dbd88624abcc0457e4e2f49515e7bc9e656ffe77
znicolaou <zack.nicolaou@gmail.com>
2020-10-27 20:52:27 -0700

    update counts

--- count.py
+++ count.py
@@ -4,7 +4,7 @@ c=2
 bins=101
 bmax=6
 
-n=50
+n=100
 for c in range(2,3):
     counts=np.zeros([bins,bins])
     avg=np.zeros([50,50,2,2])
@@ -14,11 +14,14 @@ for c in range(2,3):
             vals=np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"evals.npy")
             counts=counts+np.histogram2d(np.real(vals),np.imag(vals),bins=bins,range=[[-bmax,bmax],[-bmax,bmax]])[0]
             tot+=len(vals)
+        except:
+            print("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"evals.npy"+" not found")
+            pass
+        try:
             vals2=np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"g.npy"),
             avg=avg+vals
-
         except:
-            print("seed "+str(seed)+" not found")
+            print("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"g.npy"+" not found")
             pass
     np.save("data/"+str(c)+"_"+str(n)+".npy",counts)
     np.save("data/"+str(c)+"_"+str(n)+"g.npy",avg)
