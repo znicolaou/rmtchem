e053fab7cc39cf137b8d97de20b7bf4b1909759f
znicolaou <zack.nicolaou@gmail.com>
2020-10-27 20:49:04 -0700

    updated count

--- count.py
+++ count.py
@@ -7,17 +7,15 @@ bmax=6
 n=50
 for c in range(2,3):
     counts=np.zeros([bins,bins])
-    avg=np.zeros([25,2,2])
+    avg=np.zeros([50,50,2,2])
     tot=0
-    for seed in range(1,1024):
+    for seed in range(1,8000):
         try:
             vals=np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"evals.npy")
             counts=counts+np.histogram2d(np.real(vals),np.imag(vals),bins=bins,range=[[-bmax,bmax],[-bmax,bmax]])[0]
             tot+=len(vals)
-            g1=np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"g.npy")
-
-            vals2=np.diagonal(np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"g.npy"),axis1=3,axis2=4)
-            avg=avg+np.mean(vals2,axis=(3))/1024
+            vals2=np.load("data/"+str(c)+"_"+str(n)+"/"+str(seed)+"g.npy"),
+            avg=avg+vals
 
         except:
             print("seed "+str(seed)+" not found")
