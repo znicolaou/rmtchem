419050962689388721adf61399e27a0693e88369
znicolaou <zack.nicolaou@gmail.com>
2020-11-02 20:58:14 -0800

    updated counts

--- count.py
+++ count.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     parser.add_argument("--filebase", type=str, required=True, dest='filebase', help='Base string for file output')
     parser.add_argument("--output", type=int, default=1, dest='output', help='1 for matrix output, 0 for none')
     parser.add_argument("--seed", type=int, default=1, dest='seed', help='Random seed for the network')
+    parser.add_argument("--n", type=int, default=100, dest='n', help='Network size')
     parser.add_argument("--zr0", type=float, default=-2, dest='zr0', help='Initial Re(z) for generalized resolvant')
     parser.add_argument("--zi0", type=float, default=-2, dest='zi0', help='Initial Im(z) for generalized resolvant')
     parser.add_argument("--zr1", type=float, default=2, dest='zr1', help='Final Re(z) for generalized resolvant')
     parser.add_argument("--zi1", type=float, default=2, dest='zi1', help='Final Im(z) for generalized resolvant')
-    parser.add_argument("--gnum", type=int, default=201, dest='gnum', help='Number of g to evaluate')
+    parser.add_argument("--gnum", type=int, default=101, dest='gnum', help='Number of g to evaluate')
     parser.add_argument("--eta", type=float, default=1e-2, dest='eta', help='Regularization parameter')
     args = parser.parse_args()
 
@@ -32,8 +33,8 @@ if __name__ == "__main__":
     for seed in range(seed):
         try:
             vals=np.load(filebase+"/"+str(seed)+"evals.npy")
-            counts=counts+np.histogram2d(np.real(vals),np.imag(vals),bins=gnum,range=[[zr0,zr1],[zi0,zi1]])[0]
-            tot+=len(vals)
+            counts=counts+np.histogram2d(np.real(vals),np.imag(vals),bins=gnum,range=[[zr0,zr1],[zi0,zi1]])[0]/len(vals)
+            tot+=1
         except:
             print(filebase+"/"+str(seed)+"evals.npy"+" not found")
             pass
