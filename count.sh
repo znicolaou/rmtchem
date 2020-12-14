d973a9c722ccaa33e9afa2354fc3116a93426184
Zachary Nicolaou <zack.nicolaou@gmail.com>
2020-12-13 22:12:32 -0500

    updated counts

--- count.sh
+++ count.sh
@@ -1,4 +1,4 @@
-ns="50 100"
+ns="50 100 200"
 
 for n in $ns; do
   echo $n
@@ -8,7 +8,7 @@ for n in $ns; do
     ds=`ls data/${n}/${c}`
     for d in $ds; do
       echo $n $c $d
-      echo $n $c `tail -n1 -q data/${n}/${c}/${d}/* | awk '{n+=$3;tot++; if ($4>0){s1++}; if ($5>0){s2++}; if($6==0){t++;tr+=$7;tp+=$8};if ($6==1){h++;hr+=$7;hp+=$8}; if($3==2){s++;sr+=$7;sp+=$8}}END{print(s1/tot,s2/tot,n/tot,h/t,s/t,tr/t,tp/t,hr/h,hp/h,sr/s,sp/s)}'` >> data/${n}_counts.txt
+      echo $n $c $d `tail -n1 -q data/${n}/${c}/${d}/* | awk 'BEGIN{n=0;tot=0;t=0;h=0;s=0;s1=0;s2=0;hr=0;hp=0;sr=0;sp=0;tr=0;tp=0}{n+=$3;tot++; if ($4>0){s1++}; if ($5>0){s2++}; if($6==0){t++;tr+=$7;tp+=$8};if ($6==1){h++;hr+=$7;hp+=$8}; if($6==2){s++;sr+=$7;sp+=$8}}END{print(s1/tot,s2/tot,n/tot,t,h,s,tr/t,tp/t,hr/h,hp/h,sr/s,sp/s)}'` >> data/${n}_counts.txt
     done
   done
 done
