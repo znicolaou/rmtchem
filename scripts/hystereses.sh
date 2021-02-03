5707e53ef9bf1b31076f6b1bf9a54d3cbb7abbdc
Zack Nicolaou <zack.nicolaou@gmail.com>
2021-02-03 15:00:21 -0800

    updated scripts

--- scripts/hystereses.sh
+++ scripts/hystereses.sh
@@ -14,26 +14,30 @@ export OMP_NUM_THREADS=1
 ZGN_num=64
 ZGN_skip=10
 ZGN_steps=5000
-ZGN_ns="50 100 200 500 1000"
+ZGN_ns="50 100"
 ZGN_cs="2 2.5 3"
 ZGN_ds="25 10 5"
+ZGN_as="0 0.1 0.2"
 
 jid=$((SLURM_ARRAY_TASK_ID-1))
 
 for n in $ZGN_ns; do
 for c in $ZGN_cs; do
 for d in $ZGN_ds; do
-echo $n $c $d
+for a in $ZGN_as; do
+echo $n $c $d $a
 #nr=$((n*c))
 nr=`bc <<< "${n}*${c} / 1"`
 nd=$((n/d))
-ZGN_filebase0="data/hystereses/${n}/${c}/${d}"
+na=$((nr*a))
+
+ZGN_filebase0="data/hystereses/${n}/${c}/${d}/${a}"
 mkdir -p $ZGN_filebase0
 
 for sid in `seq $ZGN_num`; do
 seed=$((ZGN_num*jid+sid))
 ZGN_filebase="${ZGN_filebase0}/${seed}"
-./rmtchem.py --filebase $ZGN_filebase --n $n --nr $nr --nd $nd --seed $seed --steps $ZGN_steps --skip $ZGN_skip &
+./rmtchem.py --filebase $ZGN_filebase --n $n --nr $nr --nd $nd --na $na --seed $seed --steps $ZGN_steps --skip $ZGN_skip &
 
 js=`jobs | wc -l`
 while [ $js -ge 16 ]; do
@@ -42,6 +46,7 @@ while [ $js -ge 16 ]; do
 done
 done
 
+done
 done
 done
 done
