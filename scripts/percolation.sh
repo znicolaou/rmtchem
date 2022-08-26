95d8f6a0b14abf8a13c031a50c5de54cf4fb97cb
Zachary Nicolaou <zack.nicolaou@gmail.com>
2022-08-26 10:29:27 -0700

    updated percolation

--- scripts/percolation.sh
+++ scripts/percolation.sh
@@ -1,20 +1,25 @@
 export OMP_NUM_THREADS=1
 
-procs=8
+if [ $# -ne 2 ]; then
+echo usage ./percolation.sh filebase atoms
+exit
+fi
+
+procs=16
 ZGN_num=1024
 ZGN_ns="64 256 1024"
-#ZGN_ns="64 128 256"
 ZGN_cmin=0.025
 ZGN_cmax=0.5
 ZGN_dc=0.025
-ZGN_atoms=0
+ZGN_atoms=$2
+ZGN_filebase00=$1
 
 for seed in `seq 1 $ZGN_num`; do
 echo $seed
 for c in `seq $ZGN_cmin $ZGN_dc $ZGN_cmax`; do
 for n in $ZGN_ns; do
 nr=`bc <<< "${n}*${c} / 1"`
-ZGN_filebase0="data/sing/${n}/${c}"
+ZGN_filebase0=${ZGN_filebase00}/${n}/${c}
 mkdir -p $ZGN_filebase0
 ZGN_filebase="${ZGN_filebase0}/${seed}"
 if [ -f ${ZGN_filebase}out.dat ]; then
